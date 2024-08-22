bb4355803011bf8fe8928eb3a4972d317ffbcda1
GuidoAmbiorix <guidoambiorix@gmail.com>
2024-08-21 22:38:17 -0400

    first commit

---
+++ 3.Pokedex/src/seed/seed.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { SeedService } from './seed.service';
+import { SeedController } from './seed.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from 'src/pokemon/entities/pokemon.entity';
+import { CommonModule } from 'src/common/common.module';
+
+@Module({
+  controllers: [SeedController],
+  providers: [SeedService],
+  imports: [
+    CommonModule,
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
+})
+export class SeedModule {}
